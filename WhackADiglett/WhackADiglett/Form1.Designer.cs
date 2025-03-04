ca2c9068c670af79005f55135781e6f007a94e57
DuckiLikesCoding <242508@student.wqe.ac.uk>
2025-03-04 23:30:26 +0000

    Add files via upload

---
+++ WhackADiglett/WhackADiglett/Form1.Designer.cs
@@ -0,0 +1,337 @@
+﻿namespace WhackADiglett
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.x3y3 = new System.Windows.Forms.PictureBox();
+            this.x2y3 = new System.Windows.Forms.PictureBox();
+            this.x1y3 = new System.Windows.Forms.PictureBox();
+            this.x0y3 = new System.Windows.Forms.PictureBox();
+            this.x3y2 = new System.Windows.Forms.PictureBox();
+            this.x2y2 = new System.Windows.Forms.PictureBox();
+            this.x1y2 = new System.Windows.Forms.PictureBox();
+            this.x0y2 = new System.Windows.Forms.PictureBox();
+            this.x3y1 = new System.Windows.Forms.PictureBox();
+            this.x2y1 = new System.Windows.Forms.PictureBox();
+            this.x1y1 = new System.Windows.Forms.PictureBox();
+            this.x0y1 = new System.Windows.Forms.PictureBox();
+            this.x3y0 = new System.Windows.Forms.PictureBox();
+            this.x2y0 = new System.Windows.Forms.PictureBox();
+            this.x1y0 = new System.Windows.Forms.PictureBox();
+            this.x0y0 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.x3y3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.x2y3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.x1y3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.x0y3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.x3y2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.x2y2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.x1y2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.x0y2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.x3y1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.x2y1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.x1y1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.x0y1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.x3y0)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.x2y0)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.x1y0)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.x0y0)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // x3y3
+            // 
+            this.x3y3.BackColor = System.Drawing.SystemColors.Control;
+            this.x3y3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("x3y3.BackgroundImage")));
+            this.x3y3.Image = ((System.Drawing.Image)(resources.GetObject("x3y3.Image")));
+            this.x3y3.Location = new System.Drawing.Point(703, 72);
+            this.x3y3.Name = "x3y3";
+            this.x3y3.Size = new System.Drawing.Size(120, 120);
+            this.x3y3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.x3y3.TabIndex = 15;
+            this.x3y3.TabStop = false;
+            this.x3y3.Click += new System.EventHandler(this.x3y3_Click);
+            // 
+            // x2y3
+            // 
+            this.x2y3.BackColor = System.Drawing.SystemColors.Control;
+            this.x2y3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("x2y3.BackgroundImage")));
+            this.x2y3.Image = ((System.Drawing.Image)(resources.GetObject("x2y3.Image")));
+            this.x2y3.Location = new System.Drawing.Point(577, 72);
+            this.x2y3.Name = "x2y3";
+            this.x2y3.Size = new System.Drawing.Size(120, 120);
+            this.x2y3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.x2y3.TabIndex = 14;
+            this.x2y3.TabStop = false;
+            this.x2y3.Click += new System.EventHandler(this.x2y3_Click);
+            // 
+            // x1y3
+            // 
+            this.x1y3.BackColor = System.Drawing.SystemColors.Control;
+            this.x1y3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("x1y3.BackgroundImage")));
+            this.x1y3.Image = ((System.Drawing.Image)(resources.GetObject("x1y3.Image")));
+            this.x1y3.Location = new System.Drawing.Point(451, 72);
+            this.x1y3.Name = "x1y3";
+            this.x1y3.Size = new System.Drawing.Size(120, 120);
+            this.x1y3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.x1y3.TabIndex = 13;
+            this.x1y3.TabStop = false;
+            this.x1y3.Click += new System.EventHandler(this.x1y3_Click);
+            // 
+            // x0y3
+            // 
+            this.x0y3.BackColor = System.Drawing.SystemColors.Control;
+            this.x0y3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("x0y3.BackgroundImage")));
+            this.x0y3.Image = ((System.Drawing.Image)(resources.GetObject("x0y3.Image")));
+            this.x0y3.Location = new System.Drawing.Point(325, 72);
+            this.x0y3.Name = "x0y3";
+            this.x0y3.Size = new System.Drawing.Size(120, 120);
+            this.x0y3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.x0y3.TabIndex = 12;
+            this.x0y3.TabStop = false;
+            this.x0y3.Click += new System.EventHandler(this.x0y3_Click);
+            // 
+            // x3y2
+            // 
+            this.x3y2.BackColor = System.Drawing.SystemColors.Control;
+            this.x3y2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("x3y2.BackgroundImage")));
+            this.x3y2.Image = ((System.Drawing.Image)(resources.GetObject("x3y2.Image")));
+            this.x3y2.Location = new System.Drawing.Point(703, 198);
+            this.x3y2.Name = "x3y2";
+            this.x3y2.Size = new System.Drawing.Size(120, 120);
+            this.x3y2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.x3y2.TabIndex = 11;
+            this.x3y2.TabStop = false;
+            this.x3y2.Click += new System.EventHandler(this.x3y2_Click);
+            // 
+            // x2y2
+            // 
+            this.x2y2.BackColor = System.Drawing.SystemColors.Control;
+            this.x2y2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("x2y2.BackgroundImage")));
+            this.x2y2.Image = ((System.Drawing.Image)(resources.GetObject("x2y2.Image")));
+            this.x2y2.Location = new System.Drawing.Point(577, 198);
+            this.x2y2.Name = "x2y2";
+            this.x2y2.Size = new System.Drawing.Size(120, 120);
+            this.x2y2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.x2y2.TabIndex = 10;
+            this.x2y2.TabStop = false;
+            this.x2y2.Click += new System.EventHandler(this.x2y2_Click);
+            // 
+            // x1y2
+            // 
+            this.x1y2.BackColor = System.Drawing.SystemColors.Control;
+            this.x1y2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("x1y2.BackgroundImage")));
+            this.x1y2.Image = ((System.Drawing.Image)(resources.GetObject("x1y2.Image")));
+            this.x1y2.Location = new System.Drawing.Point(451, 198);
+            this.x1y2.Name = "x1y2";
+            this.x1y2.Size = new System.Drawing.Size(120, 120);
+            this.x1y2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.x1y2.TabIndex = 9;
+            this.x1y2.TabStop = false;
+            this.x1y2.Click += new System.EventHandler(this.x1y2_Click);
+            // 
+            // x0y2
+            // 
+            this.x0y2.BackColor = System.Drawing.SystemColors.Control;
+            this.x0y2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("x0y2.BackgroundImage")));
+            this.x0y2.Image = ((System.Drawing.Image)(resources.GetObject("x0y2.Image")));
+            this.x0y2.Location = new System.Drawing.Point(325, 198);
+            this.x0y2.Name = "x0y2";
+            this.x0y2.Size = new System.Drawing.Size(120, 120);
+            this.x0y2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.x0y2.TabIndex = 8;
+            this.x0y2.TabStop = false;
+            this.x0y2.Click += new System.EventHandler(this.x0y2_Click);
+            // 
+            // x3y1
+            // 
+            this.x3y1.BackColor = System.Drawing.SystemColors.Control;
+            this.x3y1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("x3y1.BackgroundImage")));
+            this.x3y1.Image = ((System.Drawing.Image)(resources.GetObject("x3y1.Image")));
+            this.x3y1.Location = new System.Drawing.Point(703, 324);
+            this.x3y1.Name = "x3y1";
+            this.x3y1.Size = new System.Drawing.Size(120, 120);
+            this.x3y1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.x3y1.TabIndex = 7;
+            this.x3y1.TabStop = false;
+            this.x3y1.Click += new System.EventHandler(this.x3y1_Click);
+            // 
+            // x2y1
+            // 
+            this.x2y1.BackColor = System.Drawing.SystemColors.Control;
+            this.x2y1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("x2y1.BackgroundImage")));
+            this.x2y1.Image = ((System.Drawing.Image)(resources.GetObject("x2y1.Image")));
+            this.x2y1.Location = new System.Drawing.Point(577, 324);
+            this.x2y1.Name = "x2y1";
+            this.x2y1.Size = new System.Drawing.Size(120, 120);
+            this.x2y1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.x2y1.TabIndex = 6;
+            this.x2y1.TabStop = false;
+            this.x2y1.Click += new System.EventHandler(this.x2y1_Click);
+            // 
+            // x1y1
+            // 
+            this.x1y1.BackColor = System.Drawing.SystemColors.Control;
+            this.x1y1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("x1y1.BackgroundImage")));
+            this.x1y1.Image = ((System.Drawing.Image)(resources.GetObject("x1y1.Image")));
+            this.x1y1.Location = new System.Drawing.Point(451, 324);
+            this.x1y1.Name = "x1y1";
+            this.x1y1.Size = new System.Drawing.Size(120, 120);
+            this.x1y1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.x1y1.TabIndex = 5;
+            this.x1y1.TabStop = false;
+            this.x1y1.Click += new System.EventHandler(this.x1y1_Click);
+            // 
+            // x0y1
+            // 
+            this.x0y1.BackColor = System.Drawing.SystemColors.Control;
+            this.x0y1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("x0y1.BackgroundImage")));
+            this.x0y1.Image = ((System.Drawing.Image)(resources.GetObject("x0y1.Image")));
+            this.x0y1.Location = new System.Drawing.Point(325, 324);
+            this.x0y1.Name = "x0y1";
+            this.x0y1.Size = new System.Drawing.Size(120, 120);
+            this.x0y1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.x0y1.TabIndex = 4;
+            this.x0y1.TabStop = false;
+            this.x0y1.Click += new System.EventHandler(this.x0y1_Click);
+            // 
+            // x3y0
+            // 
+            this.x3y0.BackColor = System.Drawing.SystemColors.Control;
+            this.x3y0.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("x3y0.BackgroundImage")));
+            this.x3y0.Image = ((System.Drawing.Image)(resources.GetObject("x3y0.Image")));
+            this.x3y0.Location = new System.Drawing.Point(703, 450);
+            this.x3y0.Name = "x3y0";
+            this.x3y0.Size = new System.Drawing.Size(120, 120);
+            this.x3y0.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.x3y0.TabIndex = 3;
+            this.x3y0.TabStop = false;
+            this.x3y0.Click += new System.EventHandler(this.x3y0_Click);
+            // 
+            // x2y0
+            // 
+            this.x2y0.BackColor = System.Drawing.SystemColors.Control;
+            this.x2y0.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("x2y0.BackgroundImage")));
+            this.x2y0.Image = ((System.Drawing.Image)(resources.GetObject("x2y0.Image")));
+            this.x2y0.Location = new System.Drawing.Point(577, 450);
+            this.x2y0.Name = "x2y0";
+            this.x2y0.Size = new System.Drawing.Size(120, 120);
+            this.x2y0.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.x2y0.TabIndex = 2;
+            this.x2y0.TabStop = false;
+            this.x2y0.Click += new System.EventHandler(this.x2y0_Click);
+            // 
+            // x1y0
+            // 
+            this.x1y0.BackColor = System.Drawing.SystemColors.Control;
+            this.x1y0.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("x1y0.BackgroundImage")));
+            this.x1y0.Image = ((System.Drawing.Image)(resources.GetObject("x1y0.Image")));
+            this.x1y0.Location = new System.Drawing.Point(451, 450);
+            this.x1y0.Name = "x1y0";
+            this.x1y0.Size = new System.Drawing.Size(120, 120);
+            this.x1y0.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.x1y0.TabIndex = 1;
+            this.x1y0.TabStop = false;
+            this.x1y0.Click += new System.EventHandler(this.x1y0_Click);
+            // 
+            // x0y0
+            // 
+            this.x0y0.BackColor = System.Drawing.SystemColors.Control;
+            this.x0y0.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("x0y0.BackgroundImage")));
+            this.x0y0.Image = ((System.Drawing.Image)(resources.GetObject("x0y0.Image")));
+            this.x0y0.Location = new System.Drawing.Point(325, 450);
+            this.x0y0.Name = "x0y0";
+            this.x0y0.Size = new System.Drawing.Size(120, 120);
+            this.x0y0.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.x0y0.TabIndex = 0;
+            this.x0y0.TabStop = false;
+            this.x0y0.Click += new System.EventHandler(this.x0y0_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1148, 642);
+            this.Controls.Add(this.x3y3);
+            this.Controls.Add(this.x2y3);
+            this.Controls.Add(this.x1y3);
+            this.Controls.Add(this.x0y3);
+            this.Controls.Add(this.x3y2);
+            this.Controls.Add(this.x2y2);
+            this.Controls.Add(this.x1y2);
+            this.Controls.Add(this.x0y2);
+            this.Controls.Add(this.x3y1);
+            this.Controls.Add(this.x2y1);
+            this.Controls.Add(this.x1y1);
+            this.Controls.Add(this.x0y1);
+            this.Controls.Add(this.x3y0);
+            this.Controls.Add(this.x2y0);
+            this.Controls.Add(this.x1y0);
+            this.Controls.Add(this.x0y0);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.x3y3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.x2y3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.x1y3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.x0y3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.x3y2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.x2y2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.x1y2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.x0y2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.x3y1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.x2y1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.x1y1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.x0y1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.x3y0)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.x2y0)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.x1y0)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.x0y0)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox x0y0;
+        private System.Windows.Forms.PictureBox x1y0;
+        private System.Windows.Forms.PictureBox x2y0;
+        private System.Windows.Forms.PictureBox x3y0;
+        private System.Windows.Forms.PictureBox x3y1;
+        private System.Windows.Forms.PictureBox x2y1;
+        private System.Windows.Forms.PictureBox x1y1;
+        private System.Windows.Forms.PictureBox x0y1;
+        private System.Windows.Forms.PictureBox x3y3;
+        private System.Windows.Forms.PictureBox x2y3;
+        private System.Windows.Forms.PictureBox x1y3;
+        private System.Windows.Forms.PictureBox x0y3;
+        private System.Windows.Forms.PictureBox x3y2;
+        private System.Windows.Forms.PictureBox x2y2;
+        private System.Windows.Forms.PictureBox x1y2;
+        private System.Windows.Forms.PictureBox x0y2;
+    }
+}
+
---
+++ WhackADiglett/WhackADiglett/Form1.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WhackADiglett
+{
+    public partial class Form1 : Form
+    {
+        static Image Full = WhackADiglett.Properties.Resources.Diglett_Front;
+        static Image Half = WhackADiglett.Properties.Resources.Diglett_Half;
+        static Image Hole = WhackADiglett.Properties.Resources.Diglett_Hole;
+        static Random random = new Random();
+        int randomX = 0;
+        int randomY = 0;
+        int diglettCount = 0;
+        int score = 0;
+        int gameSpeed = 3000; //how long it takes in between each diglett popping up
+        int diglettUpTime = 5000; //how long diglett remains up
+        int disappearAnimSpeed = 50; //diglett disappearing after being hit speed (simulates it being whacked)
+        int appearAnimSpeed = 500; //how fast the diglett comes out of the hole
+
+
+        public  Form1()
+        {
+            InitializeComponent();
+
+            for (int x = 0; x <= 3; x++) // x0 to x3
+            {
+                for (int y = 0; y <= 3; y++) // y0 to y3
+                {
+                    // create the picture box name 
+                    string pictureBoxName = $"x{x}y{y}";
+
+                    // Find the PictureBox by name
+                    Control control = this.Controls[pictureBoxName];
+                    if (control is PictureBox pictureBox)
+                    {
+                        pictureBox.Image = Hole;
+                    }
+                }
+            }
+
+            StartLoop();
+        }
+
+        private async void StartLoop()
+        {
+            while (true)
+            {
+                if (diglettCount < 16)
+                {
+                    string boxName = RandomCoordinate();
+
+                    Control control = this.Controls[boxName];
+
+                    if (control is PictureBox pictureBox)
+                    {
+                        if (pictureBox.Image != null && pictureBox.Image == Hole)
+                        {
+                            DiglettAnimation(pictureBox);
+                            await Task.Delay(gameSpeed);
+                        }
+                    }
+                }
+                else
+                { 
+                    await Task.Delay(gameSpeed);
+                }
+            }
+        }
+        private string RandomCoordinate()
+        {
+            randomX = random.Next(0, 4);
+            randomY = random.Next(0, 4);
+
+            return $"x{randomX}y{randomY}";
+        }
+        private async void DiglettAnimation(PictureBox imageBox)
+        {
+            if (imageBox.Tag is Timer existingTimer) //if the tag has a timer, reference this timer with existingTimer
+            {
+                existingTimer.Stop(); //stop timer
+                existingTimer.Dispose(); //remove timer
+                imageBox.Tag = null; //ensure it is removed
+            }
+
+            Image originalState = imageBox.Image; //store original before animating
+
+            if (imageBox.Image == Half)
+            {
+                await Task.Delay(disappearAnimSpeed);
+                if (imageBox.Image != Half) return;
+                imageBox.Image = Hole;
+                diglettCount--;
+            }
+
+            else if (imageBox.Image == Full)
+            {
+                imageBox.Image = Half;
+                await Task.Delay(disappearAnimSpeed);
+                if (imageBox.Image != Half) return;
+                imageBox.Image = Hole;
+                diglettCount--;
+            }
+
+            else if (imageBox.Image == Hole)
+            {
+                diglettCount++;
+                imageBox.Image = Half;
+                await Task.Delay(appearAnimSpeed);
+                if (imageBox.Image != Half) return; //if during the delay image != half, stop going through with this
+                imageBox.Image = Full;
+                StartDisappearanceTimer(imageBox);
+            }
+        }
+        private void StartDisappearanceTimer(PictureBox imageBox)
+        {
+            Timer diglettTimer = new Timer();
+            diglettTimer.Interval = diglettUpTime;
+            diglettTimer.Tag = imageBox; // Associate the PictureBox with the timer
+
+            diglettTimer.Tick += (sender, e) => // for every time tick goes up, do the code below
+            {
+                // Check if the Diglett is still visible
+                if (imageBox.Image == Full)
+                {
+                    disappearAnimSpeed = 250;
+                    DiglettAnimation(imageBox);
+                    disappearAnimSpeed = 50;
+                }
+
+                // Stop and dispose of the timer
+                diglettTimer.Stop();
+                diglettTimer.Dispose();
+                imageBox.Tag = null; //clear reference to avoid memory leaks
+            };
+
+            imageBox.Tag = diglettTimer; // store timer inside the picture box to be able to stop by referring to the box
+            diglettTimer.Start(); // Start the timer
+        }
+
+        private void x0y0_Click(object sender, EventArgs e)
+        {
+            if (x0y0.Image == Full || x0y0.Image == Half) { DiglettAnimation(x0y0);}
+        }
+        private void x1y0_Click(object sender, EventArgs e)
+        {
+            if (x1y0.Image == Full || x1y0.Image == Half) { DiglettAnimation(x1y0); }
+        }
+        private void x2y0_Click(object sender, EventArgs e)
+        {
+            if (x2y0.Image == Full || x2y0.Image == Half) { DiglettAnimation(x2y0); }
+        }
+        private void x3y0_Click(object sender, EventArgs e)
+        {
+            if (x3y0.Image == Full || x3y0.Image == Half) { DiglettAnimation(x3y0); }
+        }
+        private void x0y1_Click(object sender, EventArgs e)
+        {
+            if (x0y1.Image == Full || x0y1.Image == Half) { DiglettAnimation(x0y1); }
+
+        }
+        private void x1y1_Click(object sender, EventArgs e)
+        {
+            if (x1y1.Image == Full || x1y1.Image == Half) { DiglettAnimation(x1y1); }
+
+        }
+        private void x2y1_Click(object sender, EventArgs e)
+        {
+            if (x2y1.Image == Full || x2y1.Image == Half) { DiglettAnimation(x2y1); }
+
+        }
+        private void x3y1_Click(object sender, EventArgs e)
+        {
+            if (x3y1.Image == Full || x3y1.Image == Half) { DiglettAnimation(x3y1); }
+
+        }
+        private void x0y2_Click(object sender, EventArgs e)
+        {
+            if (x0y2.Image == Full || x0y2.Image == Half) { DiglettAnimation(x0y2); }
+        }
+        private void x1y2_Click(object sender, EventArgs e)
+        {
+            if (x1y2.Image == Full || x1y2.Image == Half) { DiglettAnimation(x1y2); }
+        }
+        private void x2y2_Click(object sender, EventArgs e)
+        {
+            if (x2y2.Image == Full || x2y2.Image == Half) { DiglettAnimation(x2y2); }
+        }
+        private void x3y2_Click(object sender, EventArgs e)
+        {
+            if (x3y2.Image == Full || x3y2.Image == Half) { DiglettAnimation(x3y2); }
+
+        }
+        private void x0y3_Click(object sender, EventArgs e)
+        {
+            if (x0y3.Image == Full || x0y3.Image == Half) { DiglettAnimation(x0y3); }
+
+        }
+        private void x1y3_Click(object sender, EventArgs e)
+        {
+            if (x1y3.Image == Full || x1y3.Image == Half) { DiglettAnimation(x1y3); }
+
+        }
+        private void x2y3_Click(object sender, EventArgs e)
+        {
+            if (x2y3.Image == Full || x2y3.Image == Half) { DiglettAnimation(x2y3); }
+
+        }
+        private void x3y3_Click(object sender, EventArgs e)
+        {
+            if (x3y3.Image == Full || x3y3.Image == Half) { DiglettAnimation(x3y3); }
+
+        }
+    }
+}
